07023eaae92f6255d88d6402c9791e9ee36e0546
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-29 21:42:11 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{	
+	char* grade = "A+";
+	string phrase = "Readers Academy";
+	int age = 18;
+	double gpa = 3.7;
+	bool isMale = true;
+	
+	
+	return 0 ;
+}
